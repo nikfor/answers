df0f278b34b70901ec2e13891e4e5b3aa146524a
nikfor <nikfor1@gmail.com>
2016-12-12 10:06:16 +0300

    add cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
 
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
   belongs_to :user
 
   validates_presence_of :body, :user_id
